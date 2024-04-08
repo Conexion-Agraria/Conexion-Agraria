64e64e6c01a4d3bfd510cbd71f0727dd66a1354a
Iván Herrera <119714354+ivancho1212@users.noreply.github.com>
2024-04-08 15:25:53 -0500

    delete coment

--- conexion_agraria/assets/js/Models/Cards.js
+++ conexion_agraria/assets/js/Models/Cards.js
@@ -1,12 +1,12 @@
 class Game {
   constructor(contGame) {
-    this.contGame = document.getElementById(contGame); // Contenedor de las cartas
-    this.contCardClass = "contCard"; // Clase de las cartas
-    this.getServer = window.location.origin; // Ruta del servidor
-    this.folderPath = "/conexion_agraria"; // Carpeta de las imágenes
-    this.serverPath = this.getServer + this.folderPath; // Ruta completa del servidor
+    this.contGame = document.getElementById(contGame); 
+    this.contCardClass = "contCard"; 
+    this.getServer = window.location.origin; 
+    this.folderPath = "/conexion_agraria"; 
+    this.serverPath = this.getServer + this.folderPath; 
     this.URL = "https://conexion-agraria-default-rtdb.firebaseio.com/Api/Properties.json"; // URL de los datos JSON
-    this.pathImg = this.serverPath + "/assets/img/memory/"; // Ruta de las imágenes
+    this.pathImg = this.serverPath + "/assets/img/memory/"; 
     this.getDataFromFirebase();
   }
 
@@ -14,8 +14,8 @@ class Game {
     fetch(this.URL)
       .then(response => response.json())
       .then(data => {
-        this.setElements(data); // Crear las cartas
-        this.addClickEventToCards(); // Agregar evento clic a las cartas
+        this.setElements(data); 
+        this.addClickEventToCards(); 
       })
       .catch(error => {
         console.error("Error fetching data from Firebase:", error);
@@ -23,18 +23,15 @@ class Game {
   }
 
   setElements(arrayJson) {
-    let cardsHtml = ""; // HTML de las cartas
+    let cardsHtml = ""; 
 
-    // Verificar si hay suficientes imágenes para crear las cartas
     if (arrayJson.length < 4) {
         console.error("No hay suficientes imágenes para crear las cartas.");
         return;
     }
 
-    // Obtener un subconjunto aleatorio de 4 imágenes únicas
     const shuffledImages = this.shuffleArray(arrayJson).slice(0, 4);
 
-    // Construir el HTML de las cartas
     shuffledImages.forEach(image => {
       cardsHtml += `
         <div class="col-md-3 mb-3 ${this.contCardClass}">
@@ -53,46 +50,35 @@ class Game {
         </div>`;
     });
 
-    // Insertar las cartas en el contenedor del juego
     this.contGame.innerHTML = `<div class="row">${cardsHtml}</div>`;
   }
 
-  // Evento click en las tarjetas
   addClickEventToCards() {
-    // Obtener todas las cartas después de que se han creado
     const cards = this.contGame.querySelectorAll('.card');
   
-    // Agregar evento clic a cada carta
     cards.forEach(card => {
       card.addEventListener('click', () => {
-        // Mostrar la información de la carta en el modal
         const modalImage = document.getElementById('modal-image');
         const modalTitle = document.getElementById('modal-title');
         
         const modalDescription = document.getElementById('modal-description');
   
-        // Obtener la información de la carta
         const nombre = card.querySelector('.card-title').textContent;
         const direccion = card.querySelector('.card-text').textContent;
-        const medidas = card.querySelector('img').dataset.medida; // Accede a la propiedad medida
+        const medidas = card.querySelector('img').dataset.medida; 
   
-        // Configurar el contenido del modal
         modalImage.src = card.querySelector('img').dataset.src;
         modalTitle.textContent = nombre;
         
-        // Mostrar solo las medidas en el modal
         modalDescription.innerHTML = `
             <p>Dirección: ${direccion}</p>
             <p>Medida: ${medidas}</p>
         `;
   
-        // Ocultar el formulario
         document.getElementById('contactFormSection').style.display = 'none';
         
-        // Mostrar la sección de la tarjeta
         document.getElementById('cardInfo').style.display = 'block';
   
-        // Mostrar el modal
         $('#gameModal').modal('show');
       });
     });
--- conexion_agraria/assets/js/main.js
+++ conexion_agraria/assets/js/main.js
@@ -1,53 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Variables globales
     var firebaseUser;
     var game;
     const contId = "containerGame";
     const firebase = new FirebaseUser();
 
-    // Define la función startGame() antes de la primera invocación
     function startGame(numCards) {
-        // Pasa el segundo argumento (numCards) al constructor de la clase Game
-        game = new Game(contId, numCards); // Aquí debes pasar el valor deseado para numCards
+        game = new Game(contId, numCards); 
         game.getDataFromFirebase();
 
-        // Instanciar la clase Game para el segundo contenedor
-        const game2 = new Game("containerGame2", numCards); // Aquí también debes pasar el mismo valor de numCards
+        const game2 = new Game("containerGame2", numCards); 
         game2.getDataFromFirebase();
     }
 
-    // Iniciar el juego automáticamente al cargar la página
-    startGame(20); // Por ejemplo, aquí se pasa 20 como el valor de numCards
+    startGame(20); 
 
-    // Añadir evento click al botón "Contactanos"
     document.getElementById("btn_start").addEventListener("click", function () {
-        // Limpiar el formulario al abrir el modal de contacto
         document.getElementById("contactForm").reset();
 
-        // Ocultar la información de la tarjeta
         document.getElementById('cardInfo').style.display = 'none';
 
-        // Mostrar el formulario
         document.getElementById('contactFormSection').style.display = 'block';
 
-        // Mostrar el modal
         $("#gameModal").modal("show");
     });
 
-    // Escuchar el evento de envío del formulario
     const formUser = document.getElementById("contactForm");
     formUser.addEventListener("submit", (e) => {
         e.preventDefault();
         console.log("Formulario enviado");
 
-        // Obtener los datos del formulario
         const nombre = document.getElementById("name").value;
         const telefono = document.getElementById("telefono").value;
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
 
-        // Crear objeto con los datos del usuario
         const userData = {
             nombre: nombre,
             telefono: telefono,
@@ -55,12 +42,9 @@ document.addEventListener("DOMContentLoaded", function () {
             descripcion: message,
         };
 
-        // Llamar al método para crear un nuevo usuario
         firebase.setCreateUser(userData).then(() => {
-            // Mostrar alerta de éxito
             alert("Mensaje enviado correctamente.");
 
-            // Cerrar modal después de crear el usuario
             $('#gameModal').modal('hide');
         }).catch(error => {
             console.error('Error al crear usuario:', error);
